2b8f6fe22ba357949c8da37390e8535f25e04c08
LinggaPurnamaAlHamdi <lingga.purnama088@gmail.com>
2021-05-29 12:54:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quiz10118038/MainActivity.java
@@ -0,0 +1,20 @@
+/**
+Tanggal pengerjaan : 29 Mei 2021
+        NIM : 10118038
+        Nama : Lingga Purnama Al Hamdi
+        Kelas : IF-1
+*/
+package com.example.quiz10118038;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
